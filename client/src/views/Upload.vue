a23be3a88df36a3727edea8f0f61e0d726547207
Your Name <you@example.com>
2021-05-20 14:59:08 +0530

    Start Work On Upload Page

---
+++ client/src/views/Upload.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="component container p-5">
+    <h1 class="text-center pt-5 p-3">UPLOAD NEW TRACK</h1>
+    <div>
+      <UploadForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import UploadForm from '@/components/uploadForm.vue';
+export default {
+  name: 'Upload',
+  components: {
+    UploadForm,
+  },
+};
+</script>

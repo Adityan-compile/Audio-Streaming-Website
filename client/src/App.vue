d71137913851afecd14bd0bec19c5ab9ff4f6ea9
Adityan <>
2021-06-02 11:52:11 +0530

    Start Work on Music Player Footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,15 +2,22 @@
   <div>
     <Navbar></Navbar>
     <router-view />
+    <MusicPlayer v-if="isLoggedIn" />
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/navbar';
+import MusicPlayer from "@/components/musicPlayer";
+import { mapGetters } from 'vuex';
 
 export default {
   components: {
     Navbar,
+    MusicPlayer
   },
+  computed: {
+    ...mapGetters('auth', ['isLoggedIn']),
+  }
 };
 </script>

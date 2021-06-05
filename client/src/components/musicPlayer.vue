96560b55068d0ec7d79f83e91b28a936d63b8b54
Your Name <you@example.com>
2021-06-05 11:09:59 +0530

    Make Music Player Responsive

--- client/src/components/musicPlayer.vue
+++ client/src/components/musicPlayer.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="music-player component box-shadow sticky-bottom">
     <div class="card-footer fixed-bottom bg-dark">
-      <div class="d-flex flex-row justify-content-center">
-        <div class="p-2">
+      <div class="d-flex flex-row justify-content-start align-items-center">
+        <div class="p-1">
           <object
-            data="http://127.0.0.1:8080/assets/images/60a3c4c7a557bb0fdc2c0291-60ba3539dac02b2f0d93d5fa-majentaj.peg"
+            data="http://127.0.0.1:8080/assets/images/60a3c4c7a557bb0fdc2c0291-60ba343ddac02b2f0d93d5f4-selfishj.peg"
             type="image/jpeg"
             height="70px"
             width="70px"
@@ -13,15 +13,20 @@
             @error.prevent="setVisibility"
           ></object>
         </div>
-        <div class="p-2 align-self-center">Magenta Riddim</div>
-        <div class="p-2 align-self-center">Dj Snake</div>
-        <div class="p-2 mt-3">
-          <audio controls ref="player" class="align-self-center shadow rounded bg-dark">
+        <div class="d-flex flex-column flex-grow p-1 pt-2 w-100">
+          <div class="flex-shrink">
+        <div>Selfish Love</div>
+        <div>Dj Snake & Selena Gomez</div>
+          </div>
+        <div class="p-2">
+          <audio controls ref="player" class="align-self-center shadow rounded bg-dark flex-grow w-100">
             <source
-              src="http://127.0.0.1:3000/streams/audio/60a3c4c7a557bb0fdc2c0291-60ba3539dac02b2f0d93d5fa-y2matecom-DJSnakeMagentaRiddim.mp3"
+              src="http://127.0.0.1:3000/streams/audio/60a3c4c7a557bb0fdc2c0291-60ba343ddac02b2f0d93d5f4-y2matecom-DJSnakeSelenaGomezSelfishLoveOfficialVideo.mp3"
               type="audio/mp3"
             />
+            <p class="text-center">Your Browser Does not Support HTML5  Audio Playback</p>
           </audio>
+        </div>
         </div>
       </div>
     </div>
@@ -71,7 +76,10 @@ export default {
 </script>
 
 <style scoped>
-.player {
-  background-color: #212529;
+
+audio::-webkit-media-controls-play-button,
+audio::-webkit-media-controls-panel {
+  background-color: #2f2f2f;
+  color: #fff;
 }
 </style>

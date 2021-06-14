768a7cd9e4381f005a8e4e46705bb9a273ec87a2
Your Name <you@example.com>
2021-06-14 10:43:15 +0530

    Improve position of play and info buttons of music card in smaller viewports

--- client/src/components/musicCard.vue
+++ client/src/components/musicCard.vue
@@ -2,7 +2,7 @@
   <div class="card shadow rounded bg-dark text-center d-flex">
     <div class="card-body">
       <div class="row">
-        <div class="col-md-3">
+        <div class="col-lg-3">
           <object
             :data="staticUrl + '/' + data.image"
             type="image/jpeg"
@@ -17,32 +17,45 @@
             <img src="../assets/default.png" class="thumbnail" />
           </object>
         </div>
-        <div class="col-md-3 p-3">
-          <router-link class="align-middle card-text text-white link" v-bind:to="`/about/${data._id}`">{{ data.title }}</router-link>
+        <div class="col-lg-3 p-3">
+          <router-link
+            class="align-middle card-text text-white link"
+            v-bind:to="`/about/${data._id}`"
+            >{{ data.title }}</router-link
+          >
         </div>
-        <div class="col-md-2 p-3">
+        <div class="col-lg-2 p-3">
           <span class="align-middle card-text">{{ data.artistName }}</span>
         </div>
-        <div class="col-md-2 p-3">
+        <div class="col-lg-2 p-3">
           <span class="align-middle card-text">{{ data.yearCreated }}</span>
         </div>
-        <div class="col-md-1 p-3">
+        <div class="col-lg-2 p-3">
+          <div class="d-flex flex-nowrap align-items-center justify-content-center">
           <i
             role="button"
-            class="fa fa-play-circle play align-middle"
+            class="fa fa-play-circle play align-middle p-2"
             @click.prevent="play"
             title="Play/Pause"
           ></i>
-        </div>
-        <div class="col-md-1 p-3">
           <router-link v-bind:to="`/about/${data._id}`" class="text-white">
-          <i
-            role="button"
-            class="fa fa-info-circle info align-middle"
-            title="Info"
-          ></i>
+            <i
+              role="button"
+              class="fa fa-info-circle info align-middle p-2"
+              title="Info"
+            ></i>
           </router-link>
+          </div>
         </div>
+        <!-- <div class="col-md-1 p-3">
+            <router-link v-bind:to="`/about/${data._id}`" class="text-white">
+              <i
+                role="button"
+                class="fa fa-info-circle info align-middle"
+                title="Info"
+              ></i>
+            </router-link>
+        </div> -->
       </div>
     </div>
   </div>
@@ -99,7 +112,7 @@ export default {
 .thumbnail:hover {
   transform: scale(1.2);
 }
-.link{
-  text-decoration: none;  
+.link {
+  text-decoration: none;
 }
 </style>

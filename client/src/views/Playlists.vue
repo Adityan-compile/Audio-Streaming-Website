34490ce27269000eb539413e753dbfd7d4669f91
Your Name <you@example.com>
2021-06-17 11:25:08 +0530

    Start Work on Playlist Search

--- client/src/views/Playlists.vue
+++ client/src/views/Playlists.vue
@@ -1,31 +1,52 @@
 <template>
   <div class="component playlists p-5 pt-5">
     <h1 class="text-center p-3 pt-5">PLAYLISTS</h1>
-   
+
     <p class="text-danger text-center">{{ error }}</p>
 
-      <div class="row">
-        <div class="col-6 col-sm-3 col-md-3 text-center p-3">
-          <figure class="figure">
-            <img
-              src="../assets/new-playlist.png"
-              class="figure-img img-fluid"
-              width="100"
-              height="100"
-              alt="New Playlist"
-              role="button"
-            />
-            <p class="figure-caption text-white">New Playlist</p>
-          </figure>
-        </div>  
-        <div
-          class="col-sm-3 col-6 text-center p-3"
-          v-for="(playlist, idx) in playlists"
-          :key="idx"
-        >
-          <PlaylistCard v-bind:title="playlist" />
+    <div class="search p-3">
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Search Playlists !!"
+          aria-label="Search Playlists"
+          v-model="query"
+          @keyup="search"
+        />
+        <div class="input-group-append">
+          <button
+            class="btn btn-outline-primary"
+            type="button"
+            @click.prevent="search"
+          >
+            Search
+          </button>
         </div>
       </div>
+    </div>
+    <div class="row">
+      <div class="col-6 col-sm-3 col-md-3 text-center p-3">
+        <figure class="figure">
+          <img
+            src="../assets/new-playlist.png"
+            class="figure-img img-fluid"
+            width="100"
+            height="100"
+            alt="New Playlist"
+            role="button"
+          />
+          <p class="figure-caption text-white">New Playlist</p>
+        </figure>
+      </div>
+      <div
+        class="col-sm-3 col-6 text-center p-3"
+        v-for="(playlist, idx) in playlists"
+        :key="idx"
+      >
+        <PlaylistCard v-bind:title="playlist" ref="playlist" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -41,32 +62,49 @@ export default {
   },
 
   setup() {
-
-    let playlists = ref([]);
+    let playlists = ref(["This is Dj Snake", "This is Selena Gomez"]);
 
     let error = ref("");
 
-    function fetchPlaylists(){
-        store.dispatch("playlists/fetchPlaylists").then((res)=>{
-            if(res.length === 0){
-                error.value = "No Playlists !!";
-                playlists.value = [];
-            }else{
-                playlists.value = res;
-            }
-        }).catch(err=>{
-            error.value = "Error Fetching Playlists !!"
+    let query = ref("");
+
+    let searchCandidates = playlists.value;
+
+    function search() {
+      let queryRegex = new RegExp(query, "i");
+      let searchResults = searchCandidates.filter((playlist) => {
+        return queryRegex.test(playlist);
+      });
+
+      console.log(searchResults);
+    }
+
+    function fetchPlaylists() {
+      store
+        .dispatch("playlists/fetchPlaylists")
+        .then((res) => {
+          if (res.length === 0) {
+            error.value = "No Playlists !!";
+            playlists = [];
+          } else {
+            playlists.value = res;
+          }
+        })
+        .catch((err) => {
+          error.value = "Error Fetching Playlists !!";
         });
     }
 
-    onMounted(()=>{
-      fetchPlaylists()
-    })
+    onMounted(() => {
+      fetchPlaylists();
+    });
 
     return {
       playlists,
       fetchPlaylists,
-      error
+      error,
+      query,
+      search,
     };
   },
 };
